e4ca58590d6b217edd621452eaa9f1019a7d9e18
grjorge <deb.devel@outlook.es>
2024-12-18 00:34:22 -0600

    Fix colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,16 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
+      transparent: "transparent",
       light: {
         100: "#eceff4",
         200: "#e5e9f0",
         300: "#d8dee9",
       },
       dark: {
-        100: "#4c566a",
-        200: "#434c5e",
-        300: "#3b4252",
-        400: "#2e3440",
+        100: "#434c5e",
+        200: "#3b4252",
+        300: "#2e3440",
       },
       red: "#bf616a",
       green: "#a3be8c",
