6edbaf34ee98afbcb4c49c3300293d59d47bf717
grjorge <deb.devel@outlook.es>
2024-12-24 23:30:18 -0600

    Changes color palettes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,21 +4,13 @@ export default {
   theme: {
     colors: {
       transparent: "transparent",
-      light: {
-        100: "#eceff4",
-        200: "#e5e9f0",
-        300: "#d8dee9",
-      },
-      dark: {
-        100: "#434c5e",
-        200: "#3b4252",
-        300: "#2e3440",
-      },
-      red: "#bf616a",
-      green: "#a3be8c",
-      yellow: "#ebcb8b",
-      purple: "#b48ead",
-      blue: "#81a1c1",
+      light: "#FFFFFF",
+      dark: "#3C3C3C",
+      yellow: "#F2E085",
+      blue: "#5A8CCF",
+      purple: "#A785F2",
+      green: "#85F2B6",
+      red: "#F28585"
     },
     extend: {},
   },
