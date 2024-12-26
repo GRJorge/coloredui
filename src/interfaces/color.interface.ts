f7fcc344db3b8655f5a7f691bfeee06b0cb2edd1
grjorge <deb.devel@outlook.es>
2024-12-26 01:03:20 -0600

    Charge palettes

---
+++ src/interfaces/color.interface.ts
@@ -0,0 +1,42 @@
+export default interface ColorSchema {
+  hex: string;
+  rgb: {
+    r: number;
+    g: number;
+    b: number;
+  };
+  hsl: {
+    h: number;
+    s: number;
+    l: number;
+  };
+  cmyk: {
+    c: number;
+    m: number;
+    y: number;
+    k: number;
+  };
+  lab: {
+    l: number;
+    a: number;
+    b: number;
+  };
+}
+export interface PaletteSchema {
+  dark: {
+    light: ColorSchema;
+    normal: ColorSchema;
+    dark: ColorSchema;
+  };
+  light: {
+    light: ColorSchema;
+    normal: ColorSchema;
+    dark: ColorSchema;
+  };
+  primary: ColorSchema;
+  secondary: ColorSchema;
+  green: ColorSchema;
+  red: ColorSchema;
+  yellow: ColorSchema;
+  views: number;
+}
